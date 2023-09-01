6b01317fd864e10706b4fe63fafa5ec6462594bb
TomasdeA <tdeaguirre@fi.uba.ar>
2023-09-01 00:22:45 +0000

    se agregan los comentarios para explicar la lógica del código

--- main.cpp
+++ main.cpp
@@ -1,3 +1,18 @@
+/**
+ * @brief Al ejemplo de ARM https://github.com/armBookCodeExamples/example_1-3
+ *  
+ * @arg none
+ * @param gasDetector El pin al que se conecta el sensor de gas (ahora un switch a 3v3)
+ * @param overTempDetector El pin al que se conecta el sensor de temperatura (ahora un switch a 3v3)
+ * @param alarmLed El LED el cual se usa como alarma.
+ *
+ * @note Asegúrese de que esté el button/switch conectado a 3v3 y a los pines D2 y D3.
+ * @note Se utiliza un pull down por ende se debe conectar a 3v3. 
+ * Si se pone Pull up, conectar a GND
+ * 
+ * @retval none.
+ */
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 #include "mbed.h"
 #include "arm_book_lib.h"
 
